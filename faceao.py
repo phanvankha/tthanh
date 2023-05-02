a0023c9a0bf1d96b7096cef1c944ad306e8ce54f
phanvankha <120581197+phanvankha@users.noreply.github.com>
2023-05-02 17:12:08 +0700

    faceao.py

--- faceao.py
+++ faceao.py
@@ -1,15 +1,1 @@
-import requests, time ,threading
-id =input("nhập id của bạn:")
-def url():
- while True:
-  url = requests.get(f'https://faceslitevn.site/grap/index.php?id={id}').json
-  print(url)
-threads=[]
-for i in range(5000):
-    kk=threading.Thread(target=url)
-    kk.start()
-  
-
-  
-
 
